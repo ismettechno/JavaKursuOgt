484454a37045afd68489f8911753a4287dceb6ef
TechnoStudy <ismettechno@gmail.com>
2026-04-14 18:26:55 +0100

    merhaba 2

--- src/Gun01/_01_Merhaba.java
+++ src/Gun01/_01_Merhaba.java
@@ -3,7 +3,7 @@ package Gun01;
 public class _01_Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
-
+        System.out.println("Merhaba 2");
 
 
     }
