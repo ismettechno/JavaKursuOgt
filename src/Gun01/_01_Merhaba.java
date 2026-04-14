7f28988b3684a0d2af9136da74ea3a47c6586e5a
TechnoStudy <ismettechno@gmail.com>
2026-04-14 18:30:17 +0100

    merhaba 3

--- src/Gun01/_01_Merhaba.java
+++ src/Gun01/_01_Merhaba.java
@@ -4,6 +4,7 @@ public class _01_Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
+        System.out.println("Merhaba 3");
 
 
     }
